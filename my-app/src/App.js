b8cd269c1582c142ab37259673b2f6e588b070bf
Davit33 <khmergamerdk33@gmail.com>
2021-02-08 14:15:50 +0700

    change text

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          fk u b ro layheang
+          fk u bro layheang
         </p>
       </header>
     </div>
