aa6ec0694fa86730b3f26aca106e134c842b4d15
Davit33 <khmergamerdk33@gmail.com>
2021-02-08 14:09:09 +0700

    merge code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My name is davit
+          fk u b ro layheang
         </p>
       </header>
     </div>
